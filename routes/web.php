c1f212409d40fd205a32999c36912e5bacbc55b5
amandio-eto <joseamandiodealmeida021099@gmail.com>
2026-01-20 13:37:32 +0900

    update new features Templeate

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Dashboard.index');
 });
 
 #Ida Nee mak Route husi Controller AuthController
