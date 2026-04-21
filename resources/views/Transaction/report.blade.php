04dfb0e617477ef72d1c012e37cc5bc7a067935e
amandio-eto <joseamandiodealmeida021099@gmail.com>
2026-04-22 01:29:31 +0900

    updayt

--- resources/views/Transaction/report.blade.php
+++ resources/views/Transaction/report.blade.php
@@ -111,10 +111,10 @@
              <i class="bi bi-file-earmark-spreadsheet-fill"></i>  EXEL
         </a>
 
-        {{-- <a href="{{ route('transactions.report.qdb') }}"
+        <a href="{{ route('qdb.import') }}"
            class="btn btn-success btn-sm shadow-sm">
             QDB Export
-        </a> --}}
+        </a>
 
     </div>
 </div>
